d6e6847e9fb3500f94ba99aec8722de8e24f3bbd
Iryna Bibik <bibik.iryna004@gmail.com>
2023-03-21 18:56:57 +0200

    feat: add logo, navigation, buttons for guest and button for talent without menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export const Header = () => {
-  return <div>Header</div>
-}
-
+	const [isUserAccount] = useState(false);
+	return (
+		<header className={styles.header}>
+			<Link to="" className={styles.logo}>
+				UPTALENT
+			</Link>
+			<div className={styles.navbar}>
+				<Link to>Talents</Link>
+			</div>
+			
+      {isUserAccount ? (
+        <div className={styles['name-button']}>
+					<Link>
+						<Button>Name</Button>
+          </Link>
+        </div>
+				) : (
+					<div className={styles.guestButtons}>
+						<Button className={styles.login}>Login</Button>
+						<Button variant = 'outlined' className={styles.signup}>SignUp</Button>
+					</div>
+				)}
+		</header>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+import App from './App';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#48BDE2"
-    },
-    secondary: {
-      main: "#FFBF5B"
-    },
-    dark: {
-      main: "#292A2D",
-      contrastText: "#fff"
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          fontFamily: "'Rubik', sans-serif",
-          fontWeight:"300",
-          borderRadius: "100px",
-          '&:hover': {
-            backgroundColor: "#fff",
-            outline: "1px solid #48BDE2" , 
-            color: "#48BDE2",
-            boxShadow: "none"
-          }
-        },
-        
-      }
-    }
-  }
+	palette: {
+		primary: {
+			main: '#48BDE2',
+		},
+		secondary: {
+			main: '#FFBF5B',
+		},
+		dark: {
+			main: '#292A2D',
+			contrastText: '#fff',
+		},
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					fontFamily: "'Rubik', sans-serif",
+					fontWeight: '300',
+					borderRadius: '100px',
+					'&:hover': {
+						backgroundColor: '#fff',
+						outline: '1px solid #48BDE2',
+						color: '#48BDE2',
+						boxShadow: 'none',
+					},
+				},
+			},
+		},
+	},
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </ThemeProvider>
-  </BrowserRouter>
-)
+	<BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<Routes>
+				<Route path="*" element={<App />} />
+			</Routes>
+		</ThemeProvider>
+	</BrowserRouter>,
+);
