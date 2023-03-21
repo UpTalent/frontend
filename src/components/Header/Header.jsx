5255fb033fb8a46ac3a445e20b26ec2b35208a03
Iryna Bibik <bibik.iryna004@gmail.com>
2023-03-21 19:46:48 +0200

    feat: add menu to talent's button on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import { Button } from '@mui/material';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 import styles from './Header.module.css';
 
 export const Header = () => {
-	const [isUserAccount] = useState(false);
+	const [isUserAccount] = useState(true);
+	const [menuVisibility, setVisibility] = useState(true);
 	return (
 		<header className={styles.header}>
 			<Link to="" className={styles.logo}>
@@ -13,19 +14,31 @@ export const Header = () => {
 			<div className={styles.navbar}>
 				<Link to>Talents</Link>
 			</div>
-			
-      {isUserAccount ? (
-        <div className={styles['name-button']}>
-					<Link>
-						<Button>Name</Button>
-          </Link>
-        </div>
-				) : (
-					<div className={styles.guestButtons}>
-						<Button className={styles.login}>Login</Button>
-						<Button variant = 'outlined' className={styles.signup}>SignUp</Button>
+
+			{isUserAccount ? (
+				<div className={styles.buttongroup}>
+					<div className={styles['name-button']}>
+						<Link>
+							<Button onClick={() => setVisibility(prev => !prev)}>Name</Button>
+						</Link>
 					</div>
-				)}
+					{menuVisibility && (
+						<div className={styles.menu}>
+							<div className={styles['menu-item']}>
+								<p>Talent's profile</p>{' '}
+							</div>
+							<div className={styles['menu-item']}>
+								<p>Log out</p>
+							</div>
+						</div>
+					)}
+				</div>
+			) : (
+				<div className={styles.guestButtons}>
+					<Button className={styles.login}>Login</Button>
+					<Button variant="outlined">SignUp</Button>
+				</div>
+			)}
 		</header>
 	);
 };
