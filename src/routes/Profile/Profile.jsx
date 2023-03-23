54d0377b84cd33a9b6b0fe534ea2a32063480647
Iryna Bibik <bibik.iryna004@gmail.com>
2023-03-23 21:09:46 +0200

    fix: /Profile delete unuseful code and change field aboutMe

--- src/routes/Profile/Profile.jsx
+++ src/routes/Profile/Profile.jsx
@@ -19,7 +19,7 @@ const testUser = {
 	skills: ['Java', 'JS', 'Time management'],
 	birthday: '1997-10-31',
 	location: 'Ukraine, Kharkiv',
-	'about me':
+	aboutMe:
 		'Highly-motivated Java Developer, with one year of experience practicing and learning Java, and connected technologies seeking a full-time position where I can apply my skills. I have an economic academic education and business processing background and some experience with it. All my experience will be useful in teamwork in software development. I often worked in a team, as a friendly, inquisitive but result-target person. That last thing helps me to deep learn English up to the Intermediate level, Java Core, Java 8, MySQL, JDBC, Hibernate, and Spring and take real pleasure from it, and I am inquisitive I can feel it in my future job.',
 	password: 'somePassword12343',
 };
@@ -27,7 +27,6 @@ const testUser = {
 export const Profile = () => {
 	const [talent] = useState(testUser);
 	const [isTalent] = useState(true);
-	useEffect(() => console.log(talent), []);
 	return (
 		<div className={styles.profile}>
 			<Banner banner={talent.banner} additionalStyle={styles.profileBanner} />
@@ -68,7 +67,7 @@ export const Profile = () => {
 			</div>
 			<div className={styles.about}>
 				<p>About me</p>
-				<b>{talent['about me']}</b>
+				<b>{talent.aboutMe}</b>
 			</div>
 			{isTalent && <CreateOutlinedIcon className={styles.pencil} />}
 		</div>
