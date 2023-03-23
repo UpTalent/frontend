ac72bbe974b90a4afcfae8dcb0709c56fa7700a2
Anastasiia <bagin.nst@gmail.com>
2023-03-23 12:43:50 +0200

    cherry-pick from 0988cce

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import "./App.css"
-
-import Home from './routes/Home/Home'
-import NotFound from './routes/NotFound'
-import AppLayout from './components/AppLayout/AppLayout'
+import React from 'react';
+import './App.css';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  )
-}
+	return (
+		<>
+			<Header />
+			<div className="appContainer">
+				<Outlet />
+			</div>
+			<Footer />
+		</>
+	);
+};
 
-export default App
+export default App;
