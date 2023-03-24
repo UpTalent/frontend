bdc1dab8aefd203ba1f764866875557df35f5088
Asya <bagin.nst@gmail.com>
2023-03-24 18:12:56 +0200

    feat: /TalentsPage created pagination

--- src/components/TalentsPage/TalentsPage.jsx
+++ src/components/TalentsPage/TalentsPage.jsx
@@ -1,7 +1,8 @@
-import { Grid } from '@mui/material';
-import React from 'react';
+import { Grid, Pagination } from '@mui/material';
+import React, { useState } from 'react';
 import styles from './TalentsPage.module.css';
 import { GeneralTalent } from './components/GeneralTalent';
+import { talensAPI } from '../../api/talentsAPI';
 
 export const TalentsPage = ({
 	content,
@@ -9,6 +10,12 @@ export const TalentsPage = ({
 	total_pages,
 	isTalent,
 }) => {
+	const [currentPage, setPage] = useState(page_number + 1);
+	const changePage = (e, page) => {
+		setPage(page);
+		talensAPI.getTalents(page - 1);
+	};
+
 	let talentsList = content.map(talent => (
 		<Grid item md={4} xs={12} key={talent.id}>
 			<GeneralTalent talent={talent} isTalent={isTalent} />
@@ -19,6 +26,14 @@ export const TalentsPage = ({
 			<Grid container rowSpacing={3} align='center'>
 				{talentsList}
 			</Grid>
+			<Pagination
+				color='primary'
+				count={total_pages}
+				page={currentPage}
+				onChange={changePage}
+				variant='outlined'
+				shape='rounded'
+			/>
 		</div>
 	);
 };
