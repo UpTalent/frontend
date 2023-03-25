8941f606bcc0b9c04a8614c09158be7d1c861382
Anastasiia <bagin.nst@gmail.com>
2023-03-25 16:05:56 +0200

    fix: /api fixed request

--- src/api/talentsAPI.js
+++ src/api/talentsAPI.js
@@ -1,14 +1,14 @@
-import axios from './index';
+import axiosInstance from './index';
 
 export const talentsAPI = {
 	async getTalents(currentPage = 0, pageSize = 9) {
 		try {
-			return await axios.get(`talents?page=${currentPage}&size=${pageSize}`);
+			return await axiosInstance.get(`talents?page=${currentPage}&size=${pageSize}`);
 		} catch (error) {
 			return console.log(error);
 		}
 	},
 	getTalent(id) {
-		return axios.get(`talents/${id}`);
+		return axiosInstance.get(`talents/${id}`);
 	},
 };
