677b41ccd000f363820d009e69102bf567c2f6c7
Iryna Bibik <bibik.iryna004@gmail.com>
2023-04-03 16:42:43 +0300

    fix: /slider add photo

--- src/components/shared/Slider/SliderTalent/SliderTalent.jsx
+++ src/components/shared/Slider/SliderTalent/SliderTalent.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import styles from './SliderTalent.module.css';
-import { Skill } from '../Skill';
+import { Skill } from '../../Skill';
+import { TalentAvatar } from '../../TalentAvatar';
 
 export const SliderTalent = ({ talent }) => {
 	const skills = talent.skills
 		.slice(0, 3)
 		.map((skill, index) => <Skill key={index} skill={skill} />);
-	const photoClass = talent.avatar ? 'null' : 'default';
 	return (
 		<div className={styles.SliderTalent}>
 			<div className={styles.talentPhoto}>
-				<img
-					src={talent.avatar}
-					alt={`${talent.firstname} ${talent.lastname}`}
-					className={styles[photoClass]}
-					onError={event => {
-						event.currentTarget.className = styles.default;
-					}}
-				/>
+				<TalentAvatar photo={talent.avatar} additionalStyle={styles.avatar} />
 			</div>
 
 			<div className={styles.talentInfo}>
