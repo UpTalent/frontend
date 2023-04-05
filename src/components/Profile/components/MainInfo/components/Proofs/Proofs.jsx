26bf61a921d7c20c506472b9c5132b1d0ec02f9b
Asya <bagin.nst@gmail.com>
2023-04-05 20:06:29 +0300

    feat: /proof making proofs

--- src/components/Profile/components/MainInfo/components/Proofs/Proofs.jsx
+++ src/components/Profile/components/MainInfo/components/Proofs/Proofs.jsx
@@ -4,7 +4,7 @@ import { Proof } from '../../../../../shared/Proof';
 export const Proofs = () => {
   const testProof={
     title: 'A boring blog about doing nothing',
-    annotation: 'How’s your creativity these days? Feel like your brain is zinging with new ideas and inspiration? Thought not.',
+    summary: 'How’s your creativity these days? Feel like your brain is zinging with new ideas and inspiration? Thought not.',
     content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec lacus ipsum. Aenean ullamcorper leo non diam sagittis fringilla. Mauris sed mi convallis, malesuada metus ac, imperdiet mauris. Phasellus id suscipit lorem. Sed efficitur, mauris vel vehicula volutpat, felis arcu maximus ipsum, vitae sagittis enim nulla sed mi. Vivamus egestas semper magna, ut semper erat ullamcorper non. Ut quis est a odio dictum malesuada.
     Morbi non enim in nibh dictum mattis eget vitae libero. Vestibulum in dictum massa. Donec commodo ullamcorper dignissim. Pellentesque sit amet odio eget diam molestie accumsan ac at erat. 
     
@@ -12,6 +12,7 @@ export const Proofs = () => {
     
     Vestibulum rhoncus volutpat urna dictum dignissim. Etiam ut justo tincidunt, pellentesque mauris in, elementum metus.
     Mauris et nulla mattis, tincidunt tellus eget, placerat turpis. Nulla feugiat elit non mauris rutrum, quis mollis felis eleifend. Donec ultrices sodales mi eu tristique....`,
+    published: 'May 3 2020'
   }
 	return (
 		<div>
--- src/components/shared/Proof/Proof.jsx
+++ src/components/shared/Proof/Proof.jsx
@@ -3,32 +3,54 @@ import {
 	AccordionDetails,
 	AccordionSummary,
 	Button,
+	IconButton,
+	Tooltip,
 } from '@mui/material';
 import React from 'react';
 import styles from './Proof.module.css';
 import icon from '../../../assets/ProofIcons/haze-dry-flowers.png';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import DeleteIcon from '@mui/icons-material/Delete';
+// import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 
-export const Proof = ({ proof }) => {
+export const Proof = ({ proof, withContent }) => {
 	return (
-		<Accordion sx={{maxWidth:'450px', padding:0}}>
+		<Accordion sx={{ maxWidth: '450px' }}>
 			<AccordionSummary
-				expandIcon={
-					<Button color='primary' variant='contained'>
-						Continue reading...
-					</Button>
-				}
-                sx={{display:'flex', flexDirection:'column'}}
+				sx={{ display: 'flex', flexDirection: 'column', margin: 0, padding: 0, '& .MuiAccordionSummary-content': {
+					margin:0
+				}}}
 			>
 				<div className={styles.General}>
 					<div className={styles.ProofTitle}>
-						<div className={styles.timeStamp}></div>
+						<div className={styles.controls}>
+							<div className={styles.timeStamp}>
+								<AccessTimeIcon color='action' />
+								{proof.published}
+							</div>
+							<div className={styles.controls1}>
+								<Tooltip title='Hide'>
+									<IconButton>
+										<VisibilityIcon color='action' />
+									</IconButton>
+								</Tooltip>
+								<Tooltip title='Delete proof'>
+									<IconButton>
+										<DeleteIcon color='action' />
+									</IconButton>
+								</Tooltip>
+							</div>
+						</div>
 						<div className={styles.title}>
 							<img src={icon} alt='' />
 							<p>{proof.title}</p>
 						</div>
 					</div>
+
 					<div className={styles.annotation}>
-						<p>{proof.annotation}</p>
+						<p>{proof.summary}</p>
 					</div>
 				</div>
 			</AccordionSummary>
