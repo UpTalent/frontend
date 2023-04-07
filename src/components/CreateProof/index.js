d1a3698b4c79d30fbdffcddce7279926798ba227
Iryna Bibik <bibik.iryna004@gmail.com>
2023-04-07 15:16:53 +0300

    feat: /createProof add modal for creating proof

---
+++ src/components/CreateProof/index.js
@@ -0,0 +1,1 @@
+export { CreateProofForm } from './CreateProofForm';
--- src/components/Profile/components/MainInfo/components/Proofs/Proofs.jsx
+++ src/components/Profile/components/MainInfo/components/Proofs/Proofs.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import { Dialog, DialogContent, TextField } from '@mui/material';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-//import styles from './Proofs.module.css';
-import CloseIcon from '@mui/icons-material/Close';
 import { Proof } from '../../../../../shared/Proof';
 import styles from '../../MainInfo.module.css';
 import { proofAPI } from '../../../../../../api/proofAPI';
-import { LinearProgress } from '@mui/material';
+import { Fab, LinearProgress } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { CreateProofForm } from '../../../../../CreateProof/CreateProofForm';
 
 export const Proofs = () => {
-	const [proof, setProof] = useState(null);
-	// const [open, setOpen] = useState(false);
-	// const handleOpen = () => setOpen(true);
-	// const handleClose = () => setOpen(false);
+  const [proof, setProof] = useState(null);
+  const navigate = useNavigate();
+	const location = useLocation();
+	
+  const modalPathname = path => {
+		navigate(`${location.pathname}/${path}`);
+	};
+
 	const getProof = async () => {
 		try {
 			const { data } = await proofAPI.getProof(1, 1);
@@ -22,78 +23,38 @@ export const Proofs = () => {
 		} catch (err) {
 			console.log(err);
 		}
-	};
+  };
 
 	useEffect(() => {
 		getProof();
 	}, []);
 
 	return (
-		<div className={styles.proofContainer}>
-			{proof ? (
-				<>
-					{/* <Button onClick={handleOpen}>Open modal</Button>
-					<Dialog open={open} onClose={handleClose}>
-						<Tabs>
-							<Tab label='WRITE' />
-							<Tab label='PREVIEW' />
-						</Tabs>
-						<DialogContent>
-							<TextField
-								margin='dense'
-								id='icon'
-								label='Choose an icon'
-								type='icon'
-							/>
-
-							<TextField
-								margin='dense'
-								id='icon'
-								label='Title of proof'
-								type='icon'
-							/>
-
-							<TextField
-								margin='dense'
-								id='icon'
-								label='Add some annotation, what your proof is about?'
-								type='icon'
-								fullWidth
-							/>
-
-							<TextField
-								margin='dense'
-								id='icon'
-								label='Content of proof'
-								type='icon'
-								fullWidth
-							/>
-
-							<Button
-								type='submit'
-								variant='contained'
-								className={styles.publishButton}
-							>
-								Publish
-							</Button>
-
-							<Button
-								type='submit'
-								variant='contained'
-								className={styles.saveButton}
-							>
-								Save changes
-							</Button>
-						</DialogContent>
-						<CloseIcon className={styles.closeIcon} onClick={handleClose} />
-					</Dialog> */}
-					<Proof proof={proof} withContent={false} showControlls={false} />
-					<Proof proof={proof} withContent={true} showControlls={true} />
-					<Proof proof={proof} withContent={true} showControlls={false} />
-				</>
-			) : (
-				<LinearProgress />
+		<>
+			<div className={styles.proofContainer}>
+				<Fab
+					color='secondary'
+					aria-label='add'
+					onClick={() => {
+						modalPathname('createProof');
+					}}
+				>
+					<AddIcon />
+				</Fab>
+				{proof ? (
+					<>
+						<Proof proof={proof} withContent={false} showControlls={false} />
+						<Proof proof={proof} withContent={true} showControlls={true} />
+						<Proof proof={proof} withContent={true} showControlls={false} />
+					</>
+				) : (
+					<LinearProgress />
+				)}
+			</div>
+			<Outlet />
+			{location.pathname.endsWith('/createProof') && (
+				<CreateProofForm/>
 			)}
-		</div>
+		</>
 	);
 };
