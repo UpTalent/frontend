f30ef087e4bd502aa9d9a59c235089d47091491d
Iryna Bibik <bibik.iryna004@gmail.com>
2023-04-16 19:07:45 +0300

    fix: /HOC fix components that use HOC

--- src/HOC/withURL.jsx
+++ src/HOC/withURL.jsx
@@ -12,25 +12,25 @@ import { CircularProgress } from '@mui/material';
 
 export const withURL = (Component, getList, nameList) => () => {
 	const [searchParams, setSearchParams] = useSearchParams();
-	const dispatch = useStoreDispatch();
-	const [alignment, setAlignment] = useState('desc');
+    const [alignment, setAlignment] = useState('desc');
+    const dispatch = useStoreDispatch();
 
 	const total_pages = useSelector(getGridTotalPages);
 	const isLoading = useSelector(pendingStatus);
 	const gridItems = useSelector(getGridItem);
 
-	const urlPage = Number(searchParams.get('page')) || 1;
+    const urlPage = Number(searchParams.get('page')) || 1;
 
 	useEffect(() => {
 		const data =
 			nameList === 'talents' ? urlPage - 1 : { page: urlPage - 1, alignment };
 		dispatch(getList(data));
 		return () => dispatch(clearList());
-	}, [urlPage]);
+	}, [urlPage, alignment]);
 
 	useEffect(() => {
 		if (urlPage < 0 || (total_pages < urlPage && total_pages !== 0)) {
-			setSearchParams({ page: '1' });
+			setSearchParams({ page: '1'});
 		}
 	});
 
