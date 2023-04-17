283f8fdcdd7b57ae0135b3b2a29a8ac9c0ec624f
Iryna Bibik <bibik.iryna004@gmail.com>
2023-04-17 13:01:57 +0300

    fix: /HOC add additionalParams

--- src/HOC/withURL.jsx
+++ src/HOC/withURL.jsx
@@ -12,14 +12,21 @@ import { CircularProgress } from '@mui/material';
 
 export const withURL = (Component, getList, nameList) => () => {
 	const [searchParams, setSearchParams] = useSearchParams();
-    const [alignment, setAlignment] = useState('desc');
-    const dispatch = useStoreDispatch();
+	const [alignment, setAlignment] = useState('desc');
+	const dispatch = useStoreDispatch();
 
 	const total_pages = useSelector(getGridTotalPages);
 	const isLoading = useSelector(pendingStatus);
 	const gridItems = useSelector(getGridItem);
 
-    const urlPage = Number(searchParams.get('page')) || 1;
+	const urlPage = Number(searchParams.get('page')) || 1;
+
+	const additionalParams = {
+		urlPage,
+		setSearchParams,
+		alignment,
+		setAlignment,
+	};
 
 	useEffect(() => {
 		const data =
@@ -30,7 +37,7 @@ export const withURL = (Component, getList, nameList) => () => {
 
 	useEffect(() => {
 		if (urlPage < 0 || (total_pages < urlPage && total_pages !== 0)) {
-			setSearchParams({ page: '1'});
+			setSearchParams({ page: '1' });
 		}
 	});
 
@@ -41,13 +48,7 @@ export const withURL = (Component, getList, nameList) => () => {
 					<CircularProgress />
 				</div>
 			) : (
-				<Component
-					total_pages={total_pages}
-					urlPage={urlPage}
-					setSearchParams={setSearchParams}
-					alignment={alignment}
-					setAlignment={setAlignment}
-				/>
+				<Component total_pages={total_pages} {...additionalParams} />
 			)}
 		</>
 	);
