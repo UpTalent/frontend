74fb4d3de5e3b589aba9dec1acbd00e09d3beb3b
asya982 <105641724+asya982@users.noreply.github.com>
2023-04-29 19:08:08 +0300

    feat: /Footer refactor footer (#160)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,44 +1,68 @@
 import React from 'react';
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/upTalent.png';
 
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<h2 className={styles.footer__header}>CONTACTS</h2>
-        
-        <div className={styles.footer__wraper}>
-			<div className={`${styles.footer__bigflex} ${styles.bigflex}`}>
-		
-				<div className={`${styles.bigflex__smallflex} ${styles.smallflex}`}>
-					<Link to="https://www.softserveinc.com/uk-ua" target="blank" className={styles.smallflex__item}>© Copyright 2023 SoftServe</Link>
+			<div className={styles.bigflex}>
+				<div className={`${styles.smallflex} ${styles.info}`}>
+					<div>
+						<img src={logo} alt='logo' />
+						<p>UPTALENT</p>
+					</div>
+					<div>
+						<Link to='' className={styles.smallflex__item}>
+							<LinkedInIcon />
+						</Link>
+						<Link to='' className={styles.smallflex__item}>
+							<FacebookIcon />
+						</Link>
+						<Link to='' className={styles.smallflex__item}>
+							<TwitterIcon />
+						</Link>
+					</div>
 				</div>
-				
-				<div className={`${styles.bigflex__smallflex} ${styles.smallflex}`}>
-					<Link to="" className={styles.smallflex__item}>LVIV HQ</Link>
-					<Link to="" className={styles.smallflex__item}>2D Sadova StreetLviv 79021</Link>
-					<Link to="" className={styles.smallflex__item}>+380-32-240-9999</Link>
-					<Link to="" className={styles.smallflex__item}>uptalentinfo@gmail.com</Link>
+
+				<div className={`${styles.smallflex} ${styles.names}`}>
+					<h3>FRONTEND</h3>
+					<p>Anastasiia Bahinska</p>
+					<p>Iryna Bibik</p>
+					<p>Artem Burakov</p>
+					<p>Glieb Lytvynenko</p>
 				</div>
-				
-				<div className={`${styles.bigflex__smallflex} ${styles.smallflex}`}> 
-					<Link to="" className={styles.smallflex__item}>USA HQ – AUSTIN</Link>
-					<Link to="" className={styles.smallflex__item}>201 W 5th StreetSuite 1550Austin, TX 78701</Link>
-					<Link to="" className={styles.smallflex__item}>+1-512-516-8880</Link>
-					<Link to="" className={styles.smallflex__item}>uptalentqa@gmail.com</Link>
+
+				<div className={`${styles.smallflex} ${styles.names}`}>
+					<h3>BACKEND</h3>
+					<p>Dmytro Teliukov</p>
+					<p>Mark Himonov</p>
+					<p>Vadym Pratsybuda</p>
+					<p>Andrii Cherkasov</p>
 				</div>
-				
-				<div className={`${styles.bigflex__smallflex} ${styles.smallflex}`}>
-					<Link to="" className={styles.smallflex__item}><LinkedInIcon/></Link>
-					<Link to="" className={styles.smallflex__item}><FacebookIcon/></Link>
-					<Link to="" className={styles.smallflex__item}><TwitterIcon/></Link>
+				<div className={`${styles.smallflex} ${styles.names}`}>
+					<h3>QA</h3>
+					<p>Artem Shulak</p>
+					<p>Myroslav Shashkin</p>
+					<p>Liliia Bozhenko</p>
+					<p>Anton Romanenko</p>
+					<p>Anton Masalov</p>
 				</div>
-			
+
+				<div className={styles.smallflex}>
+					<Link
+						to='https://www.softserveinc.com/uk-ua'
+						target='blank'
+						className={styles.smallflex__item}
+					>
+						© Copyright 2023 SoftServe
+					</Link>
+				</div>
 			</div>
-    	</div>
 		</footer>
 	);
 };
