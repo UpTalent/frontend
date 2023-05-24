88273fe0378a1a3d6d61ad9e8f712431da42e535
Asya <bagin.nst@gmail.com>
2023-05-24 18:29:20 +0300

    feat: /Rank added more pleasant style to ranksInfo

--- src/components/Profile/components/Statistics/components/TotalKudos/components/Rank/RankInfoModal.jsx
+++ src/components/Profile/components/Statistics/components/TotalKudos/components/Rank/RankInfoModal.jsx
@@ -1,21 +1,27 @@
 import { Dialog } from '@mui/material';
 import React from 'react';
 import styles from './Rank.module.css';
+import { talentRank } from '../../../../../../../../assets/static/talentRank';
 
 export const RankInfoModal = ({ showInfo, setShowInfo }) => {
 	return (
 		<Dialog open={showInfo} onClick={() => setShowInfo(false)}>
 			<div className={styles.container}>
 				<h3>
-					On our site we have rank system which is based on talent's total kudos
-					count
+					On our site we have <b> rank system</b> which is based on talent's
+					total kudos count
 				</h3>
-				<ol>
-					<li><b>Trainee:</b> 0 - 10000 kudos</li>
-					<li><b>Silver:</b> 10000 - 100000 kudos</li>
-					<li><b>Gold:</b> 100000 - 1000000 kudos</li>
-					<li><b>Platinum Cat:</b> 1000000+ kudos</li>
-				</ol>
+				<div>
+					{talentRank.map((rank, index) => (
+						<div className={styles.rankItem}>
+							<h3>{rank.rank}</h3>
+							<b>
+								{rank.kudos} - {talentRank[index + 1]?.kudos || 'and more'}{' '}
+								Kudos
+							</b>
+						</div>
+					))}
+				</div>
 			</div>
 		</Dialog>
 	);
