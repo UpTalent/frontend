afed149da1f714dbaaf835aec1397d847bf23052
Iryna Bibik <bibik.iryna004@gmail.com>
2023-06-11 17:29:08 +0300

    set up cosmos and add HOC for fixtures

--- src/components/shared/Proof/components/ProofTitle/components/Kudos/components/KudosSelect/KudosSelect.fixture.js
+++ src/components/shared/Proof/components/ProofTitle/components/Kudos/components/KudosSelect/KudosSelect.fixture.js
@@ -1,6 +1,18 @@
-import React from "react";
-import { KudosSelect } from "./KudosSelect";
+import React from 'react';
+import { KudosSelect } from './KudosSelect';
+import { forCosmos } from '../../../../../../../../../service/HOC/forCosmos';
 
-export default function Kudosselect() {
-    return <KudosSelect open={true} skills={[{id:1, name: 'Java'}, {id:2, name: 'React'}, {id:3, name: 'Js'}]}/>
-}
+const Kudosselect = () => {
+	return (
+		<KudosSelect
+			open={true}
+			skills={[
+				{ id: 1, name: 'Java' },
+				{ id: 2, name: 'React' },
+				{ id: 3, name: 'Js' },
+			]}
+		/>
+	);
+};
+
+export default forCosmos(Kudosselect);
