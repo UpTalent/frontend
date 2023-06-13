f8b9eb90c8aefd206a5a3ec75bdb3b5866ace68d
Asya <bagin.nst@gmail.com>
2023-06-13 22:12:51 +0300

    feat: /HOC tested and fixed withAuth hoc

--- src/service/HOC/withAuth.js
+++ src/service/HOC/withAuth.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { getIsAuth } from '../../redux/reducers/authentification';
 import { useNavigate } from 'react-router-dom';
 
-export const withAuth = (Component, ...props) => {
-	const isAuth = useSelector(getIsAuth);
-	const navigate = useNavigate();
+export const withAuth =
+	(Component, ...props) =>
+	() => {
+		const isAuth = useSelector(getIsAuth);
+		const navigate = useNavigate();
 
-	if (!isAuth) {
-		navigate('/login');
-	}
+		useEffect(() => {
+			if (!isAuth) {
+				navigate('/home/login');
+			}
+		}, []);
 
-	return <Component {...props} />;
-};
+		return <Component {...props} />;
+	};
