3bd5a38eefaa75ffcdf2cc02de791f074be30bc4
asya982 <105641724+asya982@users.noreply.github.com>
2023-06-14 19:25:44 +0300

    Feature 6.0.1 with auth (#233)
    * feat: /HOC added withAuth hoc
    
    * feat: /HOC tested and fixed withAuth hoc

---
+++ src/service/HOC/withAuth.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { getIsAuth } from '../../redux/reducers/authentification';
+import { useNavigate } from 'react-router-dom';
+
+export const withAuth =
+	(Component, ...props) =>
+	() => {
+		const isAuth = useSelector(getIsAuth);
+		const navigate = useNavigate();
+
+		useEffect(() => {
+			if (!isAuth) {
+				navigate('/home/login');
+			}
+		}, []);
+
+		return <Component {...props} />;
+	};
